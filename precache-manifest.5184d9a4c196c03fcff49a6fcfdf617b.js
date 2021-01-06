2ebd30b9d739fbc1c621afb3904336267b066cc3
sonicjh36 <sonicjh36@hotmail.com>
2021-01-06 16:16:05 +0800

    Updates

---
+++ precache-manifest.5184d9a4c196c03fcff49a6fcfdf617b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae6d49de61afd461918fd6761005bdd",
+    "url": "/FindingCats/index.html"
+  },
+  {
+    "revision": "8c71233395449ea61219",
+    "url": "/FindingCats/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "e5fb6aba57baf994d0e6",
+    "url": "/FindingCats/static/css/main.2a5b1218.chunk.css"
+  },
+  {
+    "revision": "8c71233395449ea61219",
+    "url": "/FindingCats/static/js/2.072f70f1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/FindingCats/static/js/2.072f70f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fb6aba57baf994d0e6",
+    "url": "/FindingCats/static/js/main.143ed4ec.chunk.js"
+  },
+  {
+    "revision": "b51c4506119cdf4ec41c",
+    "url": "/FindingCats/static/js/runtime-main.88492c6a.js"
+  },
+  {
+    "revision": "5cd9ce5bb44a69ea5ecbeeb76643f5d6",
+    "url": "/FindingCats/static/media/BD_Cartoon_Shout.5cd9ce5b.ttf"
+  }
+]);
